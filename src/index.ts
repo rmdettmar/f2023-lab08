36225c311e1c00f1964f1b07372da638d82829d1
rmdettmar <rmdettmar@gmail.com>
2023-11-03 18:02:48 -0400

    moved to await/async model

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,24 @@ main([
 // Implement the async version of the above here
 // Your version should not use .then and should use try/catch instead of .catch
 async function mainAsync(fileNames: string[]): Promise<void> {
-    // Your code here
+    fileNames.forEach(async (fileName: string) => {
+        try {
+            console.log(`Running logo detection on ${fileName}`)
+            let [result] = await client.logoDetection(fileName);
+            let scores: number[] = [];
+            const logos = result.logoAnnotations;
+            logos?.forEach((logo) => {
+                if (logo.description) 
+                    console.log(`"${logo.description}" found in in file ${fileName}`);
+                if (logo.score)
+                    scores.push(logo.score);
+            });
+            const avg = scores.reduce((a, b) => a + b) / scores.length;
+            console.log(`Average score for ${fileName}: ${avg}`);
+        } catch (e) {
+            if ((e as any).code === 'ENOENT') console.log(`File ${fileName} not found`);
+        }; 
+    });
 }
 
 mainAsync([
